9c1fa57aeae5088a4242c324838819fcea9fc648
Ayomide Oludairo <oludairoa@berea.edu>
2026-09-06 17:18:32 +0000

    Question 1 response updated

--- coding.3/quest1.c
+++ coding.3/quest1.c
@@ -22,3 +22,14 @@ int main() {
     }
     return 0;
 }
+
+/*********************************************
+   Your name: Ayomide Oludairo
+   Question 1: 
+
+   In the child process x becomes 101. In the
+   parent process x becomes 102. Since I
+   created a fork, each process has its own
+   copy of x, so changing x in one process
+   has no effect on the x in the other.
+*********************************************/
