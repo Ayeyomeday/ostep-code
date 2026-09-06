5861ddc56dbeebebdd445cf73169b5509ba4a1c4
Ayomide Oludairo <oludairoa@berea.edu>
2026-09-06 08:53:30 +0000

    Completed question 2

---
+++ coding.3/quest2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int main() {
+
+    int file = open("random.txt", O_WRONLY | O_CREAT | O_APPEND);
+    int rc = fork();
+
+    if (rc < 0) {
+        printf("failed to fork\n");
+    } else if (rc == 0) {
+        write(file, "I am the Child\n", 15);
+    } else {
+        write(file, "I am the Parent\n", 16);
+    }
+
+    close(file);
+    return 0;
+}
