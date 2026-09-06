2b9fe148ce466303e4f64ab6bbedee038157259f
Ayomide Oludairo <oludairoa@berea.edu>
2026-09-06 17:28:21 +0000

    Question 6 modification w/ waitpid() complete.

---
+++ coding.3/quest6.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int main() {
+
+    int rc = fork();
+
+    if (rc < 0) {
+        printf("failed to fork\n");
+    } else if (rc == 0) {
+        int rc_wait = waitpid(rc, NULL, 0);
+        printf("hello\n");
+        printf("parent of %d (rc_wait:%d) (pid:%d)\n", 
+            rc, rc_wait, (int) getpid());
+    } else {
+        printf("goodbye\n");
+                int rc_wait = waitpid(rc, NULL, 0);
+        printf("hello\n");
+        printf("parent of %d (rc_wait:%d) (pid:%d)\n", 
+            rc, rc_wait, (int) getpid());
+    }
+    return 0;
+}
+
+/*********************************************
+   Your name: Ayomide Oludairo
+   Question 6: 
+
+   waitpid() is useful for when you want to
+   wait for a specific child to finish its 
+   running process.
+*********************************************/
